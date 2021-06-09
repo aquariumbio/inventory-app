23df3a61a0e947c7c1738b77f982641bd05b4851
Devin Strickland <strcklnd@uw.edu>
2021-06-09 01:19:59 +0000

    more has_many and dependent destroy to models that were failing

--- inventory/app/models/collection.rb
+++ inventory/app/models/collection.rb
@@ -1,4 +1,5 @@
 class Collection < ApplicationRecord
   belongs_to :collection_type
   has_many :items, as: :format, :dependent => :destroy
+  has_many :collection_items, :dependent => :destroy
 end
--- inventory/app/models/container_type.rb
+++ inventory/app/models/container_type.rb
@@ -1,3 +1,4 @@
 class ContainerType < ApplicationRecord
   has_many :containers, :dependent => :destroy
+  has_many :item_types, :dependent => :destroy
 end
